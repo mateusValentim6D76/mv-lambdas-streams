eb7e0c7d5a848fd04884b0bda03cf7effa79cc8b
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-29 01:17:25 -0300

    Insert annotation JsonIgnoreProperties

--- src/main/java/br/com/mv/learning/mvlambdasstreams/domain/SeriesData.java
+++ src/main/java/br/com/mv/learning/mvlambdasstreams/domain/SeriesData.java
@@ -1,7 +1,9 @@
 package br.com.mv.learning.mvlambdasstreams.domain;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SeriesData(@JsonAlias("Title") String title,
                          @JsonAlias("totalSeasons") String totalSeasons,
                          @JsonAlias("imdbRating") String imdbRating) {
