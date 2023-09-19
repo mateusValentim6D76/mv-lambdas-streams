06839e9e894675e7aeccf122839057f9d101b77f
mateusValentim <mateus.valentim19@hotmail.com>
2023-09-18 22:21:51 -0300

    implements Record EpisodesData

---
+++ src/main/java/br/com/mv/learning/mvlambdasstreams/domain/EpisodesData.java
@@ -0,0 +1,10 @@
+package br.com.mv.learning.mvlambdasstreams.domain;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodesData(@JsonAlias("Title") String title,
+                           @JsonAlias("Released") String released,
+                           @JsonAlias("imdbRating") String imdbRating) {
+}
